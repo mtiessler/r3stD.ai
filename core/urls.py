cc260cada7fed1037c21f00a4187dc4b3d019850
Max <max.tiessler@estudiantat.upc.edu>
2022-04-30 01:36:50 +0200

    tryout done

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/tryout', views.home())
+    path('tryout', views.home)
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
 def home(request):
     return render(request, "core/tryout.html")
