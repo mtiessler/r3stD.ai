c315c6a6bb3716dee0b6aaf0edbb3d5f897ea12b
miguelmaring <miguelmaring@gmail.com>
2022-04-30 13:29:27 +0200

    frontend casi desarrollao

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('upload', views.upload),
     path('', views.home, name='home'),
-    path('about', views.about, name='about'),
+    path('aboutUs', views.about, name='aboutus'),
     path('documentation', views.docs, name='docs'),
     path('results', views.results, name='results')
 ]
