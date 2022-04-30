1a61f62bc73cfb927b412de23ae101662f838592
Max <max.tiessler@estudiantat.upc.edu>
2022-04-30 10:52:54 +0200

    landing page ok

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('tryout', views.home),
-    path('upload', views.upload)
+    path('upload', views.upload),
+    path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('documentation', views.docs, name='docs'),
+    path('results', views.results, name='results')
 ]
