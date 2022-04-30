e904f729e41ada880ced3dad8a9c5f6c5ecd08cc
Max <max.tiessler@estudiantat.upc.edu>
2022-04-30 17:19:32 +0200

    added both types of uploads

--- core/views.py
+++ core/views.py
@@ -9,7 +9,7 @@ import json
 
 INTERNAL = '0c8d-79-156-141-48.eu.ngrok.io'
 OUT_PATH = 'C:\\Users\\Max\Desktop\\hackupc2022\\3dmodule\\data\\nerf\\userScene\\images'
-
+TRANSFORMS_PATH = r"C:\Users\Max\Desktop\hackupc2022\3dmodule\data\nerf\userScene\transforms.json"
 
 @csrf_exempt
 def upload(request):
@@ -23,16 +23,23 @@ def upload(request):
             mytrans = request.FILES['transforms']
             fs = FileSystemStorage()
             transform = fs.save(mytrans.name, mytrans)
-            shutil.move('media/'+mytrans.name, r"C:\Users\Max\Desktop\hackupc2022\3dmodule\data\nerf\userScene\transforms.json")
+            shutil.move('media/'+mytrans.name, TRANSFORMS_PATH)
         video2frames(myvideo.name, mytrans is not None)
+
+        if mytrans is None:
+            os.system('python C:/Users/Max/Desktop/hackupc2022/3dmodule/scripts/colmap2nerf.py --run_colmap --images '+OUT_PATH+' --aabb_scale 2 --out '+ TRANSFORMS_PATH)
+        
+        
         os.system('python 3dmodule/scripts/run.py --scene C:\\Users\\Max\\Desktop\\hackupc2022\\3dmodule\\data\\nerf\\userScene --mode nerf --n_steps 2000 --save_mesh output.obj --near_distance 0.5')
+        
 
         files = os.listdir(OUT_PATH)
 
         if mytrans is not None:
-            os.remove(r"C:\Users\Max\Desktop\hackupc2022\3dmodule\data\nerf\userScene\transforms.json")
+            os.remove(TRANSFORMS_PATH)
         for file in files:
             os.remove(OUT_PATH+"\\"+file)
+        
     else:
         return HttpResponse("<h1>FILE NOT UPLOADED OK</h1>")
 
@@ -58,7 +65,7 @@ def video2frames(name, hasTransform):
 
     transformFiles = []
     if hasTransform:
-        file = open(r"C:\Users\Max\Desktop\hackupc2022\3dmodule\data\nerf\userScene\transforms.json", 'r')
+        file = open(TRANSFORMS_PATH, 'r')
         data = file.read()
         file.close()
         data = json.loads(data)
@@ -81,7 +88,7 @@ def video2frames(name, hasTransform):
         # Save Frame by Frame into disk using imwrite method
         print("Saving frame: ", i)
         if hasTransform:
-            cv2.imwrite(OUT_PATH+'\\'+transformFiles[i]+".jpg", frame)
+            cv2.imwrite(OUT_PATH+'\\'+transformFiles[i], frame)
         else:
             cv2.imwrite(OUT_PATH+'\\Frame{:05d}.jpg'.format(i), frame)
         i += 1
