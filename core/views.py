7e5a14bfd08f81f199cf5b77eab3e15dd27b21df
Max <max.tiessler@estudiantat.upc.edu>
2022-04-30 07:37:44 +0200

    flask out

--- core/views.py
+++ core/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse 
 from django.core.files.storage import FileSystemStorage
 from django.views.decorators.csrf import csrf_exempt
+import requests
+
+INTERNAL = '0c8d-79-156-141-48.eu.ngrok.io'
 
 def home(request):
     return render(request, "core/tryout.html")
@@ -11,6 +14,10 @@ def upload(request):
         myfile = request.FILES['file']
         fs = FileSystemStorage()
         filename = fs.save(myfile.name, myfile)
+        files = {'file': myfile}
+        response = requests.post('https://'+INTERNAL+'/uploader', files=files)
+        print(response.content)
+        filename = fs.save(response.name, response)
         return HttpResponse("<h1>FILE UPLOADED OK</h1>")
     else:
         return HttpResponse("<h1>FILE NOT UPLOADED OK</h1>")
